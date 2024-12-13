743ab430a9d086cb9e9a516884db7e59927f6852
Caroline Pereira <103532041+caahp@users.noreply.github.com>
2024-12-13 17:36:12 -0400

    handle error - db file

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,22 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   password: 'Luke202@', 
   database: 'device_management',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+});
+
+// Verify the connection
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error while connecting to the database: ' + err.stack);
+    return;
+  }
+  console.log('Connected to the database with id ' + connection.threadId);
 });
 
 module.exports = pool;
