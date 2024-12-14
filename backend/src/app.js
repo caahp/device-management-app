b394a5733e4aec4555bb55d0058fcbdc075c6980
Caroline Pereira <103532041+caahp@users.noreply.github.com>
2024-12-14 19:59:03 -0400

    adding cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const categoryRoutes = require('./routes/CategoryRoutes');
 const deviceRoutes = require('./routes/DeviceRoutes');
+const cors = require('cors');
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 // Routes
 app.use('/categories', categoryRoutes);
