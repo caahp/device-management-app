4711a231d303c4afd41a228a2bd79b5be4e94f37
Caroline Pereira <103532041+caahp@users.noreply.github.com>
2024-12-14 21:38:05 -0400

    dashboard

--- frontend/device_management/src/app/dashboard/dashboard.component.ts
+++ frontend/device_management/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../enviroments/enviroment';
+
+
+interface Category {
+  id: number;
+  name: string;
+}
+
+interface Device {
+  id: number;
+  name: string;
+  categoryId: number;
+}
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  url = environment.apiUrl;
+  totalDevices = 0;
+  totalCategories = 0;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getTotalDevices();
+    this.getTotalCategories();
+  }
+
+  getTotalDevices(): void {
+    this.http.get<Device[]>(`${this.url}/devices`).subscribe((devices) => {
+      this.totalDevices = devices.length;
+    });
+  }
+
+  getTotalCategories(): void {
+    this.http.get<Category[]>(`${this.url}/categories`).subscribe((categories) => {
+      this.totalCategories = categories.length;
+    });
+  }
+  
 
 }
